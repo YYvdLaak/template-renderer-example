415f648f97c4bfa8b2a2d638c7243a807ca7de3e
Yannick van der Laak <yannick@levarne.nl>
2022-06-08 14:10:20 +0200

    added some comments

--- demo-fe-app/demo-react.js
+++ demo-fe-app/demo-react.js
@@ -4,17 +4,28 @@ let template = `
   <p>{{saucijsPrice}}</p>
   <p>{{quantity}}</p>
 `;
+
 let data = {
   saucijsPrice: 3,
   quantity: 10
 };
 
+// Implement your reactive system here!
+
+
+
+/**
+ * HELPER FUNCTIONS
+ */
+
+// Render the template with dynamic data in the html page
 function render() {
   let localTemplate = replaceVarsTemplate(template, data);
   $("#reactive-app").empty();
   $("#reactive-app").append(localTemplate);
 }
 
+// Replace the vars in the template ( {{ }} ) by their dynamic data fields from data{}
 function replaceVarsTemplate(text, object) {
   Object.entries(object).forEach(([key, value]) => {
     const regex = new RegExp(`{{${key}}}`);
@@ -26,10 +37,12 @@ function replaceVarsTemplate(text, object) {
   return text;
 }
 
+// Event handler to set saucijsPrice field from input
 function setSaucijsPrice(e) {
   data.saucijsPrice = e.target.value;
 }
 
+// Event handler to set quantity field from input
 function setQuantity(e) {
   data.quantity = e.target.value;
 }
